5f3ccdd8db5007f6413eeff5e6d2e30c3f273159
Matthew Wiewel <mjvwiewel@gmail.com>
2019-09-17 21:46:33 -0500

    Add files via upload

--- debugModeTest.cpp
+++ debugModeTest.cpp
@@ -7,6 +7,10 @@ int main(int argc, char* argv[])
 {
 	debugMode = checkDebugMode(argc, argv);
 	
+	printf("\nWelcome to the debugMode Test.");
+	printf("\nThere should be two statements below signed by"
+		   " \n-debugPrintf and -debugStdErr if you have activated"
+		   " \ndebugMode with the -d or -D flag on the command line.");
 	debugPrintf("\nThe debug mode has been turned on. -debugPrintf");
 	debugStdErr("\nThe debug mode has been turned on. -debugStdErr");
 	
