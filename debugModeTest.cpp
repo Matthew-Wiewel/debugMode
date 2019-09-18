bfe4061811e37ddb37db2397d6559e4f77d7a283
Matthew Wiewel <mjvwiewel@gmail.com>
2019-09-17 21:49:09 -0500

    Add files via upload

--- debugModeTest.cpp
+++ debugModeTest.cpp
@@ -1,4 +1,5 @@
 #include "debugMode.h"
+#include <iostream>
 using namespace std;
 
 bool debugMode = false;
@@ -7,10 +8,10 @@ int main(int argc, char* argv[])
 {
 	debugMode = checkDebugMode(argc, argv);
 	
-	printf("\nWelcome to the debugMode Test.");
-	printf("\nThere should be two statements below signed by"
-		   " \n-debugPrintf and -debugStdErr if you have activated"
-		   " \ndebugMode with the -d or -D flag on the command line.");
+	cout << "\nWelcome to the debugMode Test."
+		 << "\nThere should be two statements below signed by" 
+		 << "\n-debugPrintf and -debugStdErr if you have activated"
+		 << "\ndebugMode with the -d or -D flag on the command line.";
 	debugPrintf("\nThe debug mode has been turned on. -debugPrintf");
 	debugStdErr("\nThe debug mode has been turned on. -debugStdErr");
 	
